cc2a9caf8c8902041bdde7103988a2e89e109560
Jesse Pecar <Jesse.Pecar@publix.com>
2021-04-12 19:45:47 -0400

    Add project files.

---
+++ Training.UI/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Training.UI.Models;
+
+namespace Training.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Module2()
+        {
+            return View("~/Views/Home/Module2.cshtml");
+        }
+
+        public IActionResult Assignment1()
+        {
+            return View("~/Views/Home/Assignment1.cshtml");
+        }
+
+        public IActionResult Assignment2()
+        {
+            return View("~/Views/Home/Assignment2.cshtml");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
