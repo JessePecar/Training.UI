ac1d77227a0c3f4d06cdffc452125f2b88f6171b
Jesse Pecar <Jesse.Pecar@publix.com>
2021-04-26 10:18:47 -0400

    added some cshtml and a new folder for the vue components

---
+++ Training.UI/Controllers/Module4Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Training.UI.Controllers
+{
+    public class Module4Controller : Controller
+    {
+        public IActionResult Module()
+        {
+            return View("~/Views/Home/Module4/Module.cshtml");
+        }
+        public IActionResult Assignment1()
+        {
+            return View("~/Views/Home/Module4/Assignment1.cshtml");
+        }
+    }
+}
