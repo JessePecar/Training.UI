692d87287546664b325a28e1a9af9055300545ef
Jesse Pecar <Jesse.Pecar@publix.com>
2021-04-26 15:24:13 -0400

    added vue via the CLI, will move to that for the rest of the project

--- Training.UI/Startup.cs
+++ Training.UI/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VueCliMiddleware;
 
 namespace Training.UI
 {
@@ -23,6 +25,7 @@ namespace Training.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSpaStaticFiles(opt => opt.RootPath = "Components");
             services.AddControllersWithViews();
         }
 
@@ -51,6 +54,14 @@ namespace Training.UI
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapToVueCliProxy(
+                    "{*path}",
+                    new SpaOptions { SourcePath = "client-app" },
+                    npmScript: (System.Diagnostics.Debugger.IsAttached) ? "serve" : null,
+                    regex: "Compiled successfully",
+                    forceKill: true
+                    );
             });
         }
     }
